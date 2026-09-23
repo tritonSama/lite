8e546dd4b7d8e3c6bb73ccf8b544701635a65bbf
tritonSama <97619606+tritonSama@users.noreply.github.com>
2026-09-23 15:01:01 -0500

    h

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.heavenlybond.hblite"
+    namespace = "heavenlybond.lite"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +18,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.heavenlybond.hblite"
+        applicationId = "heavenlybond.lite"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
